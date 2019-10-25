3597e11a7bbb96f63ec7b5178638a11b2405ce58
b3ll3o <andreazzi-leonardo@hotmail.com>
2019-10-24 21:33:40 -0300

    lidando com as entradas dos usuarios

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,20 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, partialize, compose } from './utils/operators.js';
 
+const operations = compose(
+  partialize(debounceTime, 500),
+  partialize(takeUntil, 3)
+);
 
+const action = operations(() =>
+  service
+    .sumItens('2143')
+    .then(console.log)
+    .catch(console.log)
+);
 
 document
   .querySelector('#myButton')
-    .onclick = () => service
-      .sumItens('2222')
-      .then(console.log)
-      .catch(console.log)
+    .onclick = action;
--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,17 +1,26 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize, compose, pipe } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
-const sumItens = code => notas => notas
-  .$flatMap(nota => nota.itens)
-  .filter(item => item.codigo == code)
-  .reduce((total, item) => total + item.valor, 0);
+const getItensFromNota = notas => notas.$flatMap(nota => nota.itens);
+const filterItensByCode = (code, itens) => itens.filter(item => item.codigo == code);
+const sumItensValue = itens => itens.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
   listAll(){
-    return fetch(API).then(handleStatus);
+    return fetch(API)
+      .then(handleStatus)
+      .catch(err => {
+        console.log(err);
+        return Promise.reject('não foi possivel obter as notas fiscais');
+      });
   },
   sumItens(code){
-    return this.listAll().then(sumItens(code));
+    const filterItens = partialize(filterItensByCode, code);
+    const sumItens = pipe(getItensFromNota, filterItens, sumItensValue);
+
+    return this.listAll()
+      .then(sumItens);
   }
 }
